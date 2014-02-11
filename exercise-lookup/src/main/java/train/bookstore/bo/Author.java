d98ffe9cc2ed8db38a0dcabd865d3e25f52250ca
lprzybylski@rsmart.com <lprzybylski@rsmart.com@b826e55c-0909-ac43-027b-9f5f7e38e600>
2014-02-11 16:26:37 +0000

    Updating exercises

---
+++ exercise-lookup/src/main/java/train/bookstore/bo/Author.java
@@ -0,0 +1,53 @@
+package train.bookstore.bo;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.rice.krad.bo.PersistableBusinessObjectBase;
+
+/**
+ * A business object that represents an Author of a Book.
+ * 
+ * @author Eric Westfall
+ */
+public class Author extends PersistableBusinessObjectBase {
+
+	private static final long serialVersionUID = 6385381349983978326L;
+
+	private Long id;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
