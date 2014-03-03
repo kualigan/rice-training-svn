50be683b8d8b1150bb7d9ed5805d591b9821b5c2
lprzybylski@rsmart.com <lprzybylski@rsmart.com@b826e55c-0909-ac43-027b-9f5f7e38e600>
2014-03-03 07:32:40 +0000

    Adding krad stuffs

--- exercise-trans-enhance/src/main/java/train/bookstore/document/BookOrderDocument.java
+++
@@ -1,7 +0,0 @@
-package train.bookstore.document;
-
-import org.kuali.rice.krad.document.TransactionalDocumentBase;
-
-public class BookOrderDocument extends TransactionalDocumentBase {
-
-}
--- exercise-trans-enhance/src/main/java/train/bookstore/document/web/BookOrderForm.java
+++
@@ -1,7 +0,0 @@
-package train.bookstore.document.web;
-
-import org.kuali.rice.kns.web.struts.form.KualiTransactionalDocumentFormBase;
-
-public class BookOrderForm extends KualiTransactionalDocumentFormBase {
-
-}
