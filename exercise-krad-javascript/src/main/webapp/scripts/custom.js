5031f2a1e00d08fb268aa9c7f20e39a76fd8546d
lprzybylski@rsmart.com <lprzybylski@rsmart.com@b826e55c-0909-ac43-027b-9f5f7e38e600>
2014-05-08 22:57:34 +0000

    Adding stuff

--- exercise-krad-javascript/src/main/webapp/scripts/custom.js
+++ exercise-krad-javascript/src/main/webapp/scripts/custom.js
@@ -1,3 +1,21 @@
 function repeat(text) {
 	console.log(text);
 }
+
+jQuery(document).ready(function() {
+    var covers = []
+    jQuery.ajax({
+        url : '/trnapp/remoting/coverService/TableData'
+        }).done(function(data) {
+            covers = JSON.parse(data).aaData
+
+            console.log("Covers are " + covers)
+            console.log(jQuery('.carousel-inner > div.item > img'))
+            jQuery('.carousel-inner > div.item > img').each(function(index, item) {
+                console.log('setting cover to ' + covers[index])
+                jQuery(item).attr('src', covers[index])
+            });    
+
+        });
+        
+})
