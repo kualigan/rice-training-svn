cb1d2f1d6d04e66352a4b1d23df125080ca9e62f
lprzybylski@rsmart.com <lprzybylski@rsmart.com@b826e55c-0909-ac43-027b-9f5f7e38e600>
2014-05-09 13:32:18 +0000

    Fixing project

--- branches/krad-training/exercise-krad-javascript/src/main/webapp/scripts/custom.js
+++ branches/krad-training/exercise-krad-javascript/src/main/webapp/scripts/custom.js
@@ -2,20 +2,19 @@ function repeat(text) {
 	console.log(text);
 }
 
-jQuery(document).ready(function() {
+ jQuery(document).ready(function() {
     var covers = []
     jQuery.ajax({
-        url : '/trnapp/remoting/coverService/TableData'
+        url : '/trnapp/remoting/coverService/Covers
         }).done(function(data) {
-            covers = JSON.parse(data).aaData
+            covers = JSON.parse(data).results
 
             console.log("Covers are " + covers)
             console.log(jQuery('.carousel-inner > div.item > img'))
             jQuery('.carousel-inner > div.item > img').each(function(index, item) {
                 console.log('setting cover to ' + covers[index])
-                jQuery(item).attr('src', covers[index])
+                jQuery(item).attr('src', covers[index].imageUrl)
             });    
 
-        });
-        
+        });        
 })
