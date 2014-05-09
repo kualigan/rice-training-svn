bd95f7afde7acf814d8c76d904df0beafb6c39db
lprzybylski@rsmart.com <lprzybylski@rsmart.com@b826e55c-0909-ac43-027b-9f5f7e38e600>
2014-05-09 15:59:06 +0000

    Adding extra stuff

--- branches/krad-training/exercise-krad-custom-component/src/main/webapp/scripts/custom.js
+++ branches/krad-training/exercise-krad-custom-component/src/main/webapp/scripts/custom.js
@@ -1,3 +1,20 @@
 function repeat(text) {
 	console.log(text);
 }
+
+ jQuery(document).ready(function() {
+    var covers = []
+    jQuery.ajax({
+        url : '/trnapp/remoting/coverService/Covers
+        }).done(function(data) {
+            covers = JSON.parse(data).results
+
+            console.log("Covers are " + covers)
+            console.log(jQuery('.carousel-inner > div.item > img'))
+            jQuery('.carousel-inner > div.item > img').each(function(index, item) {
+                console.log('setting cover to ' + covers[index])
+                jQuery(item).attr('src', covers[index].imageUrl)
+            });    
+
+        });        
+})
